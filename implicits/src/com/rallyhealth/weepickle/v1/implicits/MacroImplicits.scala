d0b0429e71788612cbe46240daebc5cad7ff182b
Doug Roper <doug.roper@rallyhealth.com>
2020-05-04 23:26:46 -0400

    WIP

--- implicits/src/com/rallyhealth/weepickle/v1/implicits/MacroImplicits.scala
+++ implicits/src/com/rallyhealth/weepickle/v1/implicits/MacroImplicits.scala
@@ -14,10 +14,19 @@ object MacroImplicits {
       )
     }
   }
+  @deprecated("Can I remove", "without breaking bin compat?")
   def applyTo[T](c: scala.reflect.macros.blackbox.Context)(implicit e: c.WeakTypeTag[T]): c.Expr[T] = {
     import c.universe._
     dieIfNothing[T](c)("To")
     c.Expr[T](q"${c.prefix}.macroTo0[$e, ${c.prefix}.To]")
+  }
+  def applyToIfNotExists[T, To](c: scala.reflect.macros.blackbox.Context)(implicit e: c.WeakTypeTag[T], t: c.WeakTypeTag[To]): c.Expr[T] = {
+    import c.universe._
+    dieIfNothing[T](c)("To")
+    c.inferImplicitValue(weakTypeOf[To]) match {
+      case EmptyTree => c.Expr[T](q"${c.prefix}.macroTo0[$e, ${c.prefix}.To]")
+      case existingImplicit => c.Expr(existingImplicit)
+    }
   }
   def applyFrom[T](c: scala.reflect.macros.blackbox.Context)(implicit e: c.WeakTypeTag[T]): c.Expr[T] = {
     import c.universe._
@@ -33,7 +42,7 @@ object MacroImplicits {
 
 }
 trait MacroImplicits { this: com.rallyhealth.weepickle.v1.core.Types =>
-  implicit def macroSingletonTo[T <: Singleton]: To[T] = macro MacroImplicits.applyTo[T]
+  implicit def macroSingletonTo[T <: Singleton]: To[T] = macro MacroImplicits.applyToIfNotExists[T, To[T]]
   implicit def macroSingletonFrom[F <: Singleton]: From[F] = macro MacroImplicits.applyFrom[F]
   implicit def macroSingletonFromTo[X <: Singleton]: FromTo[X] = macro MacroImplicits.applyFromTo[X]
   def macroFrom[F]: From[F] = macro MacroImplicits.applyFrom[F]
---
+++ weepickle/test/src/com/rallyhealth/weepickle/v1/EnumeratumTests.scala
@@ -0,0 +1,61 @@
+package com.rallyhealth.weepickle.v1
+
+import com.rallyhealth.weejson.v1.{Num, WeeJson}
+import com.rallyhealth.weepickle.v1.WeePickle._
+import utest._
+
+import scala.reflect.{ClassTag, classTag}
+
+// enumeratum stuff
+trait EnumEntry {
+
+  def entryName: String
+}
+
+trait Enum[T <: EnumEntry] {
+
+  def values: Seq[T]
+}
+
+trait WeePickleEnum[Fruit <: EnumEntry] extends Enum[Fruit] {
+
+  implicit def peachPickler[Peach <: Fruit : ClassTag]: WeePickle.FromTo[Peach] = {
+    val expectedType = classTag[Peach].runtimeClass.getSimpleName
+    fromTo[String]
+      .bimap[Peach](
+        _.entryName,
+        s => values.find(_.entryName == s).get match {
+          case peach: Peach => peach
+          case notPeach => throw new Exception(s"Expected $expectedType, got $notPeach")
+        }
+      )
+  }
+}
+
+sealed abstract class Fruit(override val entryName: String) extends EnumEntry
+
+object Fruit extends WeePickleEnum[Fruit] {
+
+  case object Peach extends Fruit("peach")
+
+  case object Pear extends Fruit("pear")
+
+  case object Strawberry extends Fruit("strawberry")
+
+  override val values = Seq(Peach, Pear, Strawberry)
+}
+
+object EnumeratumTests extends TestSuite {
+
+  val tests = Tests {
+
+    test("to") {
+      test("Fruit") {
+        implicitly[To[Fruit]].visitString("peach") ==> Fruit.Peach
+      }
+      test("Peach") {
+        implicitly[To[Fruit.Peach.type]].visitString("peach") ==> Fruit.Peach
+      }
+    }
+  }
+}
